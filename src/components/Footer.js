5038d633216400ac6d2d7918a15d9ade8f1d4903
Jovan <jovan.m.nixon@gmail.com>
2021-07-15 00:06:44 -0600

    Links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,41 +1,29 @@
 import React from "react";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
 
-import { Person, Mail } from '@material-ui/icons'
-import GitHubIcon from '@material-ui/icons/GitHub';
-import LinkedInIcon from '@material-ui/icons/LinkedIn';
-import MusicNoteIcon from '@material-ui/icons/MusicNote';
 
 function Footer() {
     return (
     <footer className="mt-5">
     <Container fluid={true}>
-        <Row className="border-top justify-content-between p-0 ">
+        <Row className="border-top justify-content-center p-0 ">
             <div className="p-0" md={3} sm={12}>
                     <div className='p-3 justify-content-center'>
                         <b>
-                        <Mail className='icon'/>
-                        <span><a href="mailto:jovan.m.nixon@gmail.com" target='_blank' rel="noreferrer">Let's Connect</a></span></b>
+                        <a href="mailto:jovan.m.nixon@gmail.com" target='_blank' rel="noreferrer"> <img src="https://img.icons8.com/ios-filled/50/000000/email-sign.png" alt="Email by Icons8" className="icon" /></a></b>
                         <b>
-                        <LinkedInIcon className='icon'/>
-                        <span><a href='https://www.linkedin.com/in/jovannixon/' target='_blank' rel="noreferrer">Linkedin</a></span></b>
+                        <a href='https://www.linkedin.com/in/jovannixon/' target='_blank' rel="noreferrer"> <img src="https://img.icons8.com/ios-filled/50/000000/linkedin-circled--v1.png" alt="LinkedIn by Icons8" className="icon" /></a></b>
                         <b>
-                        <GitHubIcon className='icon'/>
-                        <span><a href='https://github.com/jmnfire' target='_blank' rel="noreferrer">GitHub</a></span></b>
+                        <a href='https://github.com/jmnfire' target='_blank' rel="noreferrer"> <img src="https://img.icons8.com/ios-filled/50/000000/github.png" alt="GitHub by Icons8" className="icon" /></a></b>
                         <b>
-                        <Person className='icon'/>
-                        <span><a href='https://docs.google.com/document/d/1azfgrbaMmYW74Wd-jkft5lVqsLgJw8NhHsc1b-vZnKc/edit' target='_blank' rel="noreferrer">Resume</a></span></b>
+                        <a href='https://docs.google.com/document/d/1azfgrbaMmYW74Wd-jkft5lVqsLgJw8NhHsc1b-vZnKc/edit' target='_blank' rel="noreferrer"> <img src="https://img.icons8.com/ios-filled/50/000000/attach-resume-female.png" alt="Resume by Icons8" className="icon" /></a></b>
                         <b>
-                        <MusicNoteIcon className='icon'/>
-                        <span><a href='https://open.spotify.com/playlist/4sLCb1NlsLI2vIgVfiW8CI' target='_blank' rel="noreferrer">Music</a></span></b>
+                        <a href='https://open.spotify.com/playlist/4sLCb1NlsLI2vIgVfiW8CI' target='_blank' rel="noreferrer"> <img src="https://img.icons8.com/ios-filled/50/000000/spotify.png" alt="Spotify by Icons8" className="icon" /></a></b>
                         
                     </div>
+                    <div className="border-top justify-content-center p-0">Hi. This site was made by ™ Jovan Nixon </div>
             </div>
-            <Col className="p-3 d-flex justify-content-end" md={3}>
-            This site was made by Jovan Nixon.
-            </Col>
         </Row>
     </Container>
     </footer>
